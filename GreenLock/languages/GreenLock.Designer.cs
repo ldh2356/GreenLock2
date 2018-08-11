ad8ad9df9e668adf9730b18a27412178cdb107bb
jinyoung <jinyoung@DESKTOP-QGTRKOT>
2018-08-11 16:15:36 +0900

    - 엑셀 수정

--- GreenLock/languages/GreenLock.Designer.cs
+++ GreenLock/languages/GreenLock.Designer.cs
@@ -19,7 +19,7 @@ namespace GreenLock.languages {
     // 클래스에서 자동으로 생성되었습니다.
     // 멤버를 추가하거나 제거하려면 .ResX 파일을 편집한 다음 /str 옵션을 사용하여 ResGen을
     // 다시 실행하거나 VS 프로젝트를 다시 빌드하십시오.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class GreenLock {
@@ -381,6 +381,24 @@ namespace GreenLock.languages {
         public static string Uc_TabSecurity_EndDate {
             get {
                 return ResourceManager.GetString("Uc_TabSecurity_EndDate", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   GreenLock Lock/UnLock Time Sheet Data과(와) 유사한 지역화된 문자열을 찾습니다.
+        /// </summary>
+        public static string Uc_TabSecurity_Excel_Sheet_Title_1 {
+            get {
+                return ResourceManager.GetString("Uc_TabSecurity_Excel_Sheet_Title_1", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Daily Lock Unlock Data과(와) 유사한 지역화된 문자열을 찾습니다.
+        /// </summary>
+        public static string Uc_TabSecurity_Excel_Sheet_Title_2 {
+            get {
+                return ResourceManager.GetString("Uc_TabSecurity_Excel_Sheet_Title_2", resourceCulture);
             }
         }
         
